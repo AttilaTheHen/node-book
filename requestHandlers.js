f3a6b71669c8e0ad3eb9c38ce2d0131393755f6e
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-03 11:27:29 -0700

    Have exec op run find to test async

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,7 +4,7 @@ function start(response) {
     console.log('Request handler \'start\' was called.');
     let content = 'empty';
 
-    exec('ls -lah', (err, stdout, stderr) => {
+    exec('find /', { timeout: 10000, maxBuffer: 20000 * 1024 }, (err, stdout, stderr) => {
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.write(stdout);
         response.end();
