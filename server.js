529b69389efc762d6e5d29d2710f15690b0e25f8
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-07-03 10:10:50 -0700

    Add eslint and gitignore and tweak server

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.write('Hello World');
+//     res.end();
+// }).listen(8888, console.log('server running...'));
+
+function onRequest(req, res) {
+    console.log('Request received.');
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Hello World');
     res.end();
-}).listen(8888, console.log('server running...'));
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('Server has started.');
